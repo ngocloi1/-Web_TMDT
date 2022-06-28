5a5dda5d493f6ff2c8de25c5136f610960bc42ab
Prince <loingocluxury@gmail.com>
2022-06-28 18:13:18 +0700

    Signed-off-by: Prince <loingocluxury@gmail.com>=.=

---
+++ resources/views/backend/layouts/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="main-footer">
+    <strong>Copyright © 2014-2022 <a href="">Admin</a>.</strong>
+    <div class="float-right d-none d-sm-inline-block">
+    </div>
+  </footer>
---
+++ resources/views/frontend/modules/about.blade.php
@@ -0,0 +1,15 @@
+@extends('frontend.layouts.master')
+@section('Content')
+
+<div class="container">
+    Người Thực Hiện : Đỗ Ngọc Lợi <br>
+    Framework Laravel <br>
+    Language PHP<br>
+    ...
+    <br>
+    <br>
+    <br>
+    <br>
+    CẢM ƠN BẠN ĐÃ GHÉ THĂM !!!
+</div>
+@endsection<br>
